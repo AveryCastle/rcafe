13e77ef627029a233d3f2063df7030be7be4a0f0
JuHyun Seong <ernestogevar@naver.com>
2015-05-19 23:10:40 +0900

    Add tag functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201200350) do
+ActiveRecord::Schema.define(version: 20150519120842) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string   "title"
